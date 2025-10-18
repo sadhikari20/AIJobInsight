2c56ac328e24356097c14d825c18e1400d37ac3b
sadhikari20 <sadhikari20@leomail.tamuc.edu>
2025-10-18 03:35:12 +0000

    Added footer

--- App.tsx
+++ App.tsx
@@ -115,7 +115,7 @@ const App: React.FC = () => {
                 </div>
                 <p className="text-slate-400 text-lg">
                     Leverage AI to get data-driven insights on skill requirements for your dream job.
-                    Note: This is a class project for BUSA 521 developed and maintained by Sujata Adhikari.
+                    
                 </p>
             </header>
 
@@ -217,6 +217,29 @@ const App: React.FC = () => {
                     )}
                 </div>
             </main>
+           
+
+<footer class="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://ai-job-insight-sigma.vercel.app/" class="hover:underline">ETAMU BUSA 521 Group 1</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">About</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline">Contact</a>
+        </li>
+    </ul>
+    </div>
+</footer>
+
         </div>
     );
 };
